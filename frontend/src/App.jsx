958d583f73bf2641e973c364b591165852a37c0b
ThinkKat <jwy06048@gmail.com>
2025-02-04 21:44:59 +0900

    Add requirement

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import SubwayMap from './components/SubwayMap/SubwayMap'
 import SidePanel from './components/SidePanel/SidePanel'
