c2be1064802ae944fbc89f6781d47dee2a2e0d66
ThinkKat <thinkcat0128@gmail.com>
2025-02-26 16:07:37 +0900

    Add restart command

--- backend/fastapi_server.sh
+++ backend/fastapi_server.sh
@@ -3,15 +3,19 @@
 case "$1" in
   dev)
     echo "Starting FastAPI in development mode..."
-    uvicorn main:app --host 127.0.0.1 --port 8001 --reload --log-config log_conf_dev.yaml
+    /home/cathink8210/projects/metro/backend/.venv/bin/uvicorn main:app --host 127.0.0.1 --port 8001 --reload --log-config log_conf_dev.yaml
     ;;
   start)
     echo "Starting FastAPI in production mode..."
-    uvicorn main:app --host 0.0.0.0 --port 8000 --log-config log_conf.yaml & disown
+    /home/cathink8210/projects/metro/backend/.venv/bin/uvicorn main:app --host 0.0.0.0 --port 8000 --log-config log_conf.yaml & disown
     ;;
   terminate)
     echo "Stopping FastAPI server..."
     kill $(ps -ef | grep 'uvicorn\smain:app' | awk '{print $2}')
+    ;;
+  restart)
+    echo "Restarting FastAPI server..."
+    kill $(ps -ef | grep 'uvicorn\smain:app' | awk '{print $2}') && /home/cathink8210/projects/metro/backend/.venv/bin/uvicorn main:app --host 0.0.0.0 --port 8000 --log-config log_conf.yaml & disown
     ;;
   *)
     echo "Usage: $0 {dev|start|terminate}"
