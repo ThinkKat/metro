c708df8e3599d202bf1dccaaa8ae8aca1d76b386
ThinkKat <thinkcat0128@gmail.com>
2025-02-27 15:25:46 +0900

    Modify the command. Path is modified

--- backend/fastapi_server.sh
+++ backend/fastapi_server.sh
@@ -3,11 +3,11 @@
 case "$1" in
   dev)
     echo "Starting FastAPI in development mode..."
-    /home/cathink8210/projects/metro/backend/.venv/bin/uvicorn main:app --host 127.0.0.1 --port 8001 --reload --log-config log_conf_dev.yaml
+    .venv/bin/uvicorn main:app --host 127.0.0.1 --port 8001 --reload --log-config log_conf_dev.yaml
     ;;
   start)
     echo "Starting FastAPI in production mode..."
-    /home/cathink8210/projects/metro/backend/.venv/bin/uvicorn main:app --host 0.0.0.0 --port 8000 --log-config log_conf.yaml & disown
+    .venv/bin/uvicorn main:app --host 0.0.0.0 --port 8000 --log-config log_conf.yaml & disown
     ;;
   terminate)
     echo "Stopping FastAPI server..."
@@ -15,7 +15,7 @@ case "$1" in
     ;;
   restart)
     echo "Restarting FastAPI server..."
-    kill $(ps -ef | grep 'uvicorn\smain:app' | awk '{print $2}') && /home/cathink8210/projects/metro/backend/.venv/bin/uvicorn main:app --host 0.0.0.0 --port 8000 --log-config log_conf.yaml & disown
+    kill $(ps -ef | grep 'uvicorn\smain:app' | awk '{print $2}') && .venv/bin/uvicorn main:app --host 0.0.0.0 --port 8000 --log-config log_conf.yaml & disown
     ;;
   *)
     echo "Usage: $0 {dev|start|terminate}"
