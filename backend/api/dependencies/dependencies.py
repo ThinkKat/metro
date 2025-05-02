dd76187ea41f443cc132f3a59471f59ef496f255
ThinkKat <jwy06048@gmail.com>
2025-05-02 20:24:05 +0900

    Add dependencies to controller. Controller module is dependent to service module and respositories module. Currently, RealtimeTransformWorker is executed in another thread in controller process. That is, RealtimeTransformWorker has an excution dependency in controller module. This structure will be divided into independent modules and communication with protocol.

--- backend/api/dependencies/dependencies.py
+++ backend/api/dependencies/dependencies.py
@@ -1,11 +1,12 @@
 import os
 
-from dotenv import load_env
+from dotenv import load_dotenv
 
+from communication.ipc_client import IPCClient
 from repositories.metro_repository.postgresql_metro_repository import PostgresqlMetroRepository
 from repositories.timetable_repository.postgresql_timetable_repository import PostgresqlTimetableRepository
 
-load_env()
+load_dotenv()
 
 async def get_metro_repository():
     repo = PostgresqlMetroRepository()
@@ -16,3 +17,4 @@ async def get_timetable_repository():
     repo = PostgresqlTimetableRepository()
     repo.create_engine(os.getenv("POSTGRESQL_METRO_DB_URL"))
     return repo
+
---
+++ backend/api/dependencies/tmp_data_client.py
@@ -0,0 +1,70 @@
+import os
+from dotenv import load_dotenv
+
+from communication.ipc_listener import IPCListener
+from communication.ipc_client import IPCClient
+
+from services.transform.src.realtime_transform_worker import RealtimeTransformWorker
+from repositories.timetable_repository.postgresql_timetable_repository import PostgresqlTimetableRepository
+from repositories.delay_repository.postgresql_delay_repository import PostgresqlDelayRepository
+from repositories.realtimes_repository.sqlite_realtime_repository import SqliteRealtimeRepository
+
+from model.pydantic_model import RealtimeArrival
+
+load_dotenv()
+tc_address = os.getenv("TRANSFORM_CONTROLLER_ADDRESS")
+ct_address = os.getenv("COLLECT_TRANSFORM_ADDRESS")
+
+metro_db_url = os.getenv("POSTGRESQL_METRO_DB_URL")
+realtime_db_url = os.getenv("SQLITE_REALTIME_DB_URL")
+
+ipc_listener = IPCListener(tc_address)
+ipc_client = IPCClient(ct_address)
+
+postgresql_timetable_repository = PostgresqlTimetableRepository()
+postgresql_delay_repository = PostgresqlDelayRepository()
+sqlite_realtime_repository = SqliteRealtimeRepository()
+
+# ipc_listener.start() # Not using
+
+postgresql_timetable_repository.create_engine(metro_db_url)
+postgresql_delay_repository.create_engine(metro_db_url)
+sqlite_realtime_repository.create_engine(realtime_db_url)
+
+realtime_transform_worker = RealtimeTransformWorker(
+    ipc_listener,
+    ipc_client,
+    postgresql_timetable_repository, 
+    postgresql_delay_repository, 
+    sqlite_realtime_repository
+)
+realtime_transform_worker.start()
+
+def get_position_by_line_id(line_id: int) -> dict:
+    return realtime_transform_worker.realtime_transform.realtime_position[line_id]
+
+def get_arrival_by_station_id(station_id: int, up: str, down: str) -> RealtimeArrival:
+        """Get arrival data by station_id 
+
+        Args:
+            station_id (int): _description_
+            up (str): _description_
+            down (str): _description_
+
+        Returns:
+            RealtimeArrival: _description_
+        """
+        
+        # Create arrival data
+        data = {"left": [], "right": []}
+        # Get data by id
+        realtime_arrival = realtime_transform_worker.realtime_transform.arrival_hashmap[station_id]
+        
+        # up: "left_direction", down: "right_direction"
+        for row in realtime_arrival:
+            if row.up_down == 0:
+                data[up.split("_")[0]].append(row)
+            else:
+                data[down.split("_")[0]].append(row)
+        return RealtimeArrival(**data)
+
