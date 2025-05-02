673d997cfae172b83b0a5dece27acc2a01eb7054
ThinkKat <jwy06048@gmail.com>
2025-05-02 21:35:07 +0900

    Change the new project directory

--- interval-collect-data/packages/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-API_KEY_DB_PATH = f"{os.getcwd()}/db/api_key.db"
-SQLITE_REALTIME_DB_PATH = f"/{os.getcwd()}/db/realtime.db"
-SQLITE_TEST_REALTIME_DB_PATH = f"/{os.getcwd()}/db/test_realtime.db"
-POSTGRESQL_METRO_DB_URL = "/metro"
-UDS_ADDRESS = '/tmp/uds_address'
-INTERVAL = 10
--- server/packages/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-TIMETABLE_DB_PATH = f"{os.getcwd()}/db/timetable.db" 
-SQLITE_REALTIME_DB_PATH = f"/{os.getcwd()}/db/realtime.db"
-SQLITE_TEST_REALTIME_DB_PATH = f"/{os.getcwd()}/db/test_realtime.db"
-POSTGRESQL_METRO_DB_URL = "/metro"
-UDS_ADDRESS = '/tmp/uds_address'
