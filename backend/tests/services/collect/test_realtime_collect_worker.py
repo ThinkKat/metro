cca42b57178d35393ea465e615ea54603ace52e5
ThinkKat <jwy06048@gmail.com>
2025-05-02 20:36:31 +0900

    Modify tests

--- backend/tests/services/collect/test_realtime_collect_worker.py
+++ backend/tests/services/collect/test_realtime_collect_worker.py
@@ -1,3 +1,5 @@
+from communication.ipc_listener import IPCListener 
+
 from services.collect.src.realtime_collect_worker import RealtimeCollectWorker
 from repositories.realtimes_repository.sqlite_realtime_repository import SqliteRealtimeRepository
 
@@ -8,11 +10,14 @@ if __name__ == "__main__":
     
     load_dotenv()
     db_url = os.getenv("SQLITE_REALTIME_DB_URL")
-    address = os.getenv("UDS_ADDRESS")
+    address = os.getenv("COLLECT_TRANSFORM_ADDRESS")
     
+    # Parameter
     interval = 10
+    ipc_listener = IPCListener(address)
+    ipc_listener.start()
     sqlite_realtime_repository = SqliteRealtimeRepository()
     sqlite_realtime_repository.create_engine(db_url)
     
-    realtime_collect_worker = RealtimeCollectWorker(address, interval, sqlite_realtime_repository)
+    realtime_collect_worker = RealtimeCollectWorker(interval, ipc_listener, sqlite_realtime_repository)
     realtime_collect_worker.start()
--- backend/tests/services/transform/test_realtime_transform_worker.py
+++ backend/tests/services/transform/test_realtime_transform_worker.py
@@ -1,3 +1,6 @@
+from communication.ipc_listener import IPCListener
+from communication.ipc_client import IPCClient
+
 from services.transform.src.realtime_transform_worker import RealtimeTransformWorker
 from repositories.timetable_repository.postgresql_timetable_repository import PostgresqlTimetableRepository
 from repositories.delay_repository.postgresql_delay_repository import PostgresqlDelayRepository
@@ -9,20 +12,28 @@ from dotenv import load_dotenv
 if __name__ == "__main__":
     
     load_dotenv()
-    address = os.getenv("UDS_ADDRESS")
+    tc_address = os.getenv("TRANSFORM_CONTROLLER_ADDRESS")
+    ct_address = os.getenv("COLLECT_TRANSFORM_ADDRESS")
+    
     metro_db_url = os.getenv("POSTGRESQL_METRO_DB_URL")
     realtime_db_url = os.getenv("SQLITE_REALTIME_DB_URL")
+    
+    ipc_listener = IPCListener(tc_address)
+    ipc_client = IPCClient(ct_address)
     
     postgresql_timetable_repository = PostgresqlTimetableRepository()
     postgresql_delay_repository = PostgresqlDelayRepository()
     sqlite_realtime_repository = SqliteRealtimeRepository()
+    
+    ipc_listener.start()
     
     postgresql_timetable_repository.create_engine(metro_db_url)
     postgresql_delay_repository.create_engine(metro_db_url)
     sqlite_realtime_repository.create_engine(realtime_db_url)
     
     realtime_transform_worker = RealtimeTransformWorker(
-        address,    
+        ipc_listener,
+        ipc_client,
         postgresql_timetable_repository, 
         postgresql_delay_repository, 
         sqlite_realtime_repository
