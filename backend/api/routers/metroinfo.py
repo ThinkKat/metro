dc2f5b10756be8253b390fb8978bbcf4340606bc
ThinkKat <jwy06048@gmail.com>
2025-05-02 20:31:03 +0900

    Modify an interface along to existing interface agreement

--- backend/api/routers/metroinfo.py
+++ backend/api/routers/metroinfo.py
@@ -1,6 +1,6 @@
 from fastapi import Depends, APIRouter
 
-from api.dependencies.repositories import get_metro_repository, get_timetable_repository
+from api.dependencies.dependencies import get_metro_repository, get_timetable_repository
 from model.pydantic_model import StationSearchbarList, StationInfo, Timetable, TimetableRow
 from utils.utils import op_date
 
@@ -12,13 +12,12 @@ async def get_stations(repo = Depends(get_metro_repository)) -> list[StationSear
     return data
 
 @router.get("/information/{station_public_code}", tags = ["metroinfo"])
-async def get_station_info(station_public_code: str, repo = Depends(get_metro_repository)) -> StationInfo:
-    data = repo.find_total_station_info(station_public_code)
+async def get_station_info(station_public_code: str,
+                           metro_repo = Depends(get_metro_repository), 
+                           tb_repo = Depends(get_timetable_repository)) -> StationInfo:
+    data = metro_repo.find_total_station_info(station_public_code)
+    data.timetables = tb_repo.find_timetable_by_station_public_code(op_date(), station_public_code)
     return data
 
-@router.get("/information/timetables/{station_public_code}", tags = ["metroinfo"])
-async def get_station_info(station_public_code: str, repo = Depends(get_timetable_repository)) -> Timetable:
-    data = repo.find_timetable_by_station_public_code(op_date(), station_public_code)
-    return data
 
 
--- backend/api/routers/realtimes.py
+++ backend/api/routers/realtimes.py
@@ -1,9 +1,18 @@
-from fastapi import APIRouter
+from fastapi import Depends, APIRouter
 
+from api.dependencies.tmp_data_client import get_position_by_line_id, get_arrival_by_station_id
+from api.dependencies.dependencies import get_metro_repository
 from model.pydantic_model import RealtimeData
 
 router = APIRouter()
 
-@router.get("/realtimes/{station_public_code}", tags = ["realtimes"])
-async def get_realtime_info(station_public_code: str) -> RealtimeData:
-    return 
+@router.get("/information/realtimes/{station_public_code}", tags = ["realtimes"])
+async def get_realtime_info(station_public_code: str, repo = Depends(get_metro_repository)) -> RealtimeData:
+    station = repo.find_station(station_public_code)
+    realtime_position = get_position_by_line_id(station["line_id"])
+    realtime_arrival = get_arrival_by_station_id(station["station_id"], station["up"], station["down"])
+    return RealtimeData(**{
+        "line": realtime_position,
+        "station": realtime_arrival
+    })
+
